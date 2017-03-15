289c51b7079dfa999d59ccd1ea0c0ded6648ea5c
Timothy Fisher <tfisher9180@gmail.com>
2017-03-15 16:57:57 -0700

    first commit

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 $(function() {
 	$(window).scroll(function() {
-		if ($(this).scrollTop() > 200) {
+		if ($(this).scrollTop() > 50) {
 			$('.navbar').addClass('navbar-slim');
 		} else {
 			$('.navbar').removeClass('navbar-slim');
